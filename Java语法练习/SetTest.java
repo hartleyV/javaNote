8d586cd11acd1d316bf758e2cd56ebc0b5f7a676
= <hartleyV@users.noreply.github.com>
2020-05-01 23:52:06 +0800

    Set集合待完成

---
+++ Java语法练习/SetTest.java
@@ -0,0 +1,21 @@
+/**
+*Set--类比于罐子，元素无序且不可重复！！
+*1 HashSet***速度快（根据哈希值索引）
+*2 TreeSet
+*3 EnumSet
+*@author Hartley、
+*/
+class Set Test 
+{
+	//元素排列顺序根据对象hashCode计算的哈希值；判断重复根据equals和hashCode方法；集合值可null
+	public static void hashSetTest()
+	{}
+	public static void treeSetTest()
+	{}
+	public static void enumSetTest()
+	{}
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
